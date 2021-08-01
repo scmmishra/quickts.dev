9b1b35fae47c3b4a9a057252fd07c32d55f66b74
Shivam Mishra <scm.mymail@gmail.com>
2021-08-01 14:11:13 +0530

    fix: better bg

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="bg-gray-500 bg-no-repeat bg-contain min-h-screen" style="background-image: url('/assets/bg.png')">
+  <div class="bg-gray-500 bg-no-repeat bg-contain min-h-screen" style="background-image: url('/assets/bg.svg')">
     <navbar />
     <hero />
     <example-selector />
     <features />
-    <div class="py-20 text-center text-gray-300">
-      Built by <a href="https://twitter.com/scmmishra" class="underline hover:text-gray-200">@scmmishra</a>, Website by
-      <a href="https://twitter.com/gajjar_devansh" class="underline hover:text-gray-200">@gajjar_devansh</a>
+    <div class="py-20 text-center text-gray-300 space-y-2">
+      <p>
+        Built by <a href="https://twitter.com/scmmishra" class="underline hover:text-gray-200">@scmmishra</a>, Website by
+        <a href="https://twitter.com/gajjar_devansh" class="underline hover:text-gray-200">@gajjar_devansh</a>
+      </p>
+      <p>Built at <a href="https://deepsource.io" class="underline hover:text-gray-200">DeepSource</a></p>
     </div>
   </div>
 </template>
