83ff4a51d74ed7f7cc1bf9af92508340d27777d2
Shivam Mishra <scm.mymail@gmail.com>
2021-08-01 13:44:12 +0530

    fix: bg contain

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-500 bg-no-repeat min-h-screen" style="background-image: url('/assets/bg.png')">
+  <div class="bg-gray-500 bg-no-repeat bg-contain min-h-screen" style="background-image: url('/assets/bg.png')">
     <navbar />
     <hero />
     <example-selector />
