ff25e90be7839e2bcd121b4d5d2d50bac007e9d2
Shivam Mishra <scm.mymail@gmail.com>
2021-08-01 13:36:21 +0530

    feat: update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/logo.svg' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/assets/favicon.svg' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
