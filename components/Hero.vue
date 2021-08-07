dbd3958725360a98007a70c545c04aefecd43835
Shivam Mishra <scm.mymail@gmail.com>
2021-08-07 10:45:48 +0530

    feat: add copy to clipboard

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,7 +5,41 @@
     </h1>
     <p class="text-white opacity-50">An opinionated but faster modern alternative to tsdx and microbundle</p>
     <div class="mt-4 flex items-center space-x-4">
-      <span class="bg-gray-900 py-4 px-6 rounded-xl text-sm text-white font-mono"> yarn global add quickts </span>
+      <div class="bg-gray-900 py-4 px-6 rounded-xl text-sm text-white font-mono flex items-center">
+        <span class="mr-3">yarn global add quickts</span>
+        <button @click="copyInstallCommand">
+          <svg
+            v-if="copying"
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-green"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6 text-primary-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+            />
+          </svg>
+        </button>
+      </div>
       <span class="text-sm text-white opacity-50">OR</span>
       <a
         href="https://github.com/scmmishra/quickts"
@@ -39,3 +73,39 @@
     </div>
   </section>
 </template>
+
+<script>
+export default {
+  name: 'Hero',
+  data() {
+    return {
+      copying: false,
+    }
+  },
+  methods: {
+    copyInstallCommand() {
+      this.copying = true
+      this.copytoclipboard('yarn global add quickts')
+      setTimeout(() => {
+        this.copying = false
+      }, 1000)
+    },
+    async copytoclipboard(candidate) {
+      try {
+        // try the clipboard API
+        // https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+        // This will fail in Firefox
+        await navigator.clipboard.writeText(candidate)
+      } catch (e) {
+        // fallback for firefox
+        // Always run inside click event
+        const el = document.createElement('input')
+        el.value = candidate
+        el.select()
+        document.execCommand('copy')
+        el.remove()
+      }
+    },
+  },
+}
+</script>
